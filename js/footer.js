825b9703b87b14c5b4fdf14f398151bf63b42606
greta-blip <54960940+greta-blip@users.noreply.github.com>
2020-03-16 20:09:52 -0400

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+
+	document.getElementById("footer").innerHTML = `
+	<footer>
+	<p>University of Michigan School of Information <br/>
+		&copy; 2020 Greta Hilburn</p>
+	</footer>`;
---
+++ js/header.js
@@ -0,0 +1,12 @@
+
+	document.getElementById("header").innerHTML = `
+	<header>
+	 <nav>
+		 <ol>
+			 <a href="index.html">Home</a>
+			 <a href="about.html" >About</a>
+			 <a href="resume.html" >Resume</a>
+			 <a href="contact.html" >Contact</a>
+		 </ol>
+	 </nav>
+	</header>`;
